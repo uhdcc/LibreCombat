76d3c6666facb669d968f838b58c0814cd6ed9ca
Cody Couch <61272065+uhdcc@users.noreply.github.com>
2024-06-10 02:11:43 -0500

    added a basic playercontroller and character

---
+++ Source/OpenCombat/PlayerController2.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerController2.h"
+
+APlayerController2::APlayerController2() {
+}
+
+void APlayerController2::BeginPlay() {
+	Super::BeginPlay();
+	SetMouseSensitivity(800.0, 16.5);
+}
+
+void APlayerController2::SetupInputComponent() {
+	Super::SetupInputComponent();
+	InputComponent->BindAxis("MouseYaw", this, &APlayerController::AddYawInput);
+	InputComponent->BindAxis("MousePitch", this, &APlayerController::AddPitchInput);
+	InputComponent->BindAxis("MoveForward", this, &APlayerController2::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &APlayerController2::MoveRight);
+}
+
+void APlayerController2::MoveForward(float Input) {
+	if(Input != 0.f) GetPawn()->AddMovementInput(GetPawn()->GetActorForwardVector(), Input);
+}
+void APlayerController2::MoveRight(float Input) {
+	if(Input != 0.f) GetPawn()->AddMovementInput(GetPawn()->GetActorRightVector(), Input);
+}
+void APlayerController2::SetMouseSensitivity(double MouseDpi, double CentimetersPer360) {
+	auto MouseSensitivity = 360.0 / MouseDpi / CentimetersPer360 * 2.54;
+	InputYawScale = MouseSensitivity;
+	InputPitchScale = MouseSensitivity;
+}
---
+++ Source/OpenCombat/PlayerController2.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerController2.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENCOMBAT_API APlayerController2 : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	APlayerController2();
+
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+	void MoveForward(float Input);
+	void MoveRight(float Input);
+
+	void SetMouseSensitivity(double MouseDpi, double CentimetersPer360);
+
+};
